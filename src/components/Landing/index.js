6046dbbd7ea464748b4f96891744cf8ed82ac1f0
eleni <eleni.aslanidou@student.kyh.se>
2020-02-19 11:57:38 +0100

    changed LandingPage.js

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+
 const LandingPage = () => (
-    <div>
-        
-    </div>
+     <div  className="container" style={{width: '100%', height: '100%', margin: 'auto', color: 'blue', fontSize: '50px'}}>
+              BEV
+            </div> 
+            
+           
     );
     export default LandingPage;
+
